cf63bf8a0ab8d5059ebf88bde250fdd4d9e4e962
Scott Albertson <ascottalbertson@gmail.com>
2018-10-29 20:26:04 -0700

    Basic retry for CompletedFileReviewJob

--- app/jobs/completed_file_review_job.rb
+++ app/jobs/completed_file_review_job.rb
@@ -10,9 +10,16 @@ class CompletedFileReviewJob < ApplicationJob
   #   [{ line: 123, message: "WAT" }]
   def self.perform(attributes)
     CompleteFileReview.call(attributes)
-  rescue ActiveRecord::RecordNotFound
-    Resque.enqueue_in(30, self, attributes)
   rescue Resque::TermException
     Resque.enqueue(self, attributes)
+  rescue ActiveRecord::RecordNotFound
+    Resque.enqueue_in(30, self, attributes)
+  rescue
+    if attributes["attempts"].to_i >= 3
+      raise
+    else
+      attributes["attempts"] = attributes["attempts"].to_i + 1
+      Resque.enqueue_in(30, self, attributes)
+    end
   end
 end
